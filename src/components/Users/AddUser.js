67bf0dbfe248d18b6613da990e4de56fe676f157
Jacques Beeets <jacques@hellofirstview.com>
2023-02-19 14:50:31 +0200

    connecting state to form input

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
+  const [userNameValue, setUserName] = useState('');
+  const [userAge, setUserAge] = useState('');
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
+  };
+
+  const userNameChangeHandler = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const userAgeChangeHandler = (event) => {
+    setUserAge(event.target.value);
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={handleFormSubmit} className="addUser">
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
-        <label htmlFor="age">Age</label>
-        <input id="age" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={userNameValue}
+          onChange={userNameChangeHandler}
+        />
+        <label htmlFor="age" value={userAge}>
+          Age
+        </label>
+        <input id="age" type="text" onChange={userAgeChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
